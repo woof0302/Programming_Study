c1cc17420f8da445de172f59317f945482ef6c54
woof0302 <woof0302@gmail.com>
2024-12-14 17:09:56 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 2.37 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define LEN_INPUT 10
+
+int main(void) {
+    char s1[LEN_INPUT] = {0};
+    scanf("%s", s1);
+
+    for(int i = 0; s1[i] != 0 ; i++)
+    {
+        if((s1[i] <= 90) && (s1[i] >64))
+        {
+            s1[i] = s1[i] + 32;
+        }
+        
+        else
+        {
+            s1[i] = s1[i] - 32;
+        }
+    }
+    
+    printf("%s", s1);
+    
+    return 0;
+}
