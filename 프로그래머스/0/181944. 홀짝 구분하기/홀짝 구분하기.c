7f4d1a48b463fb1a8dd514a3ad312a5b0ca2a30c
woof0302 <woof0302@gmail.com>
2024-12-14 17:51:42 +0900

    [level 0] Title: 홀짝 구분하기, Time: 2.46 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    scanf("%d", &a);
+
+    if(a % 2)
+    {
+        printf("%d is odd", a);
+    }
+    
+    else
+    {
+        printf("%d is even", a);
+    }
+    return 0;
+}
