c5500b2ced3b61dd5a39fcd309cba9cf614515e4
woof0302 <woof0302@gmail.com>
2025-01-26 20:27:01 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.20 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// arr_len은 배열 arr의 길이입니다.
+int* solution(int arr[], size_t arr_len, int n) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+ int* answer = (int*)malloc(sizeof(int) * arr_len);
+    
+    if(arr_len % 2 == 1)
+    {
+        for(int i = 0; i < arr_len; i++)
+        {
+            if(i % 2 == 0)
+            {
+                arr[i] += n;   
+            }            
+        }                
+    }
+    
+    else
+    {
+        for(int i = 0; i < arr_len; i++)
+        {
+            if(i % 2 == 1)
+            {
+                arr[i] += n;   
+            }            
+        }
+    }
+    
+    for(int i = 0; i < arr_len; i++)
+    {
+        answer[i] = arr[i];
+    }
+    return answer;
+} 
+
