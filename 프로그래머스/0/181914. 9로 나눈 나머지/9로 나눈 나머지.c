03f12dcc05e870d603cd6fd8d3f713141ebcf259
woof0302 <woof0302@gmail.com>
2025-03-12 21:38:21 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.10 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* number) {
+    int sum = 0;
+    
+    while(*number != '\0')
+    {
+        sum += *number - '0';
+        number++;
+    }
+    return sum % 9;
+}
